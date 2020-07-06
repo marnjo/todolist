268d7c2eb1ffb96897b86894d30050d21e85ea42
Marnjo <marnjo.coolio8000@gmail.com>
2020-07-06 16:20:37 +0300

    added additional js functionality

--- js/app.js
+++ js/app.js
@@ -2,44 +2,37 @@ let arr = []
 
 function idGen(arr){
   let id = Math.random() * 1000;
-  if(! arr.includes(id)){
+  if (! arr.includes(id) || arr.length){
     arr.push(id);
-      
-  }else{idGen()}
+  }else{idGen(arr)}
+  return id;
 }
 
 let toDoObj = [
-  {id:(Math.random()*10000), content:'Brush teeth', checked:false}
+  {id:idGen(arr), content:'Brush teeth', checked:false}
 ]
 
 const todos = document.querySelector('.todos');
 const toDoAdder = document.querySelector('button');
 const todoval = document.querySelector('.addInput');
 
-
-function update(task){
-  toDoObj.push({ id: toDoObj.length, content:task, checked:false})
-}
-
-function removed(id){
-
-}
-
 function addToDo(task){
-  update(task)
+  let id = idGen(arr)
+  toDoObj.push({ id: id, content: task, checked: false })
 
   let innerDiv = document.createElement('div');
+  innerDiv.classList.add('innerDiv');
 
   let todo = document.createElement('div'); // create the to do
   todo.classList.add('todo'); // give it a class
-  todo.classList.add(`checkmark${toDoObj.length}`); // give it a class
+  todo.classList.add(`checkmark${id}`); // give it a class
 
   let input = document.createElement('input');// create the check box
   input.setAttribute('type', 'checkbox'); // give the input an attribute of checkbox
-  input.setAttribute('id', `checkmark${toDoObj.length}`);
+  input.setAttribute('id', `checkmark${id}`);
 
   let labell = document.createElement('label');
-  labell.setAttribute('for', `checkmark${toDoObj.length}`);
+  labell.setAttribute('for', `checkmark${id}`);
   labell.innerText = task;
 
   let ptodo = document.createElement('p'); // create the p tag
@@ -47,8 +40,21 @@ function addToDo(task){
 
   ptodo.innerText = 'remove';
   ptodo.addEventListener('click', () => {
-    console.log(ptodo.parentNode)
-    ptodo.parentNode.parentNode.removeChild(ptodo.parentNode)
+    ptodo.parentNode.parentNode.removeChild(ptodo.parentNode);
+    console.log(toDoObj, arr)
+    arr = arr.filter(i => i !== id)
+    toDoObj = toDoObj.filter(j => j['id'] !== id)
+    console.log(toDoObj, arr)
+  })
+
+  input.addEventListener('click', () => {
+    labell.classList.toggle('done');
+    if(input.checked == true){
+      toDoObj.forEach(elem =>{
+        elem['checked'] = true;
+        console.log(elem)
+      })
+    }
   })
 
   innerDiv.appendChild(input);
@@ -66,3 +72,13 @@ toDoAdder.addEventListener('click', ()=> {
   addToDo(todoval.value);
   todoval.value = "";
 });
+
+document.querySelector('.clear').addEventListener('click', ()=>{
+  let arrr = Array.from(document.body.children[0].children[2].children);
+  arrr.forEach( elem => {
+    toDoObj.forEach( element =>{
+      if (element['id'] == Number(elem.classList[1].slice(9,)) && (element['checked'] == true) )
+      document.body.children[0].children[2].removeChild(elem)
+    })
+  })
+})
